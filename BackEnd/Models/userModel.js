9178820dfa23262cfe3033c2b835ce30bde544c9
EngAhmedElBayoumi <Ahmedtareq@std.mans.edu.eg>
2023-05-26 21:09:21 +0300

    user schema

---
+++ BackEnd/Models/userModel.js
@@ -0,0 +1,42 @@
+
+const mongoose = require('mongoose');
+
+const Autoincrement = require('mongoose-sequence')(mongoose);
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    id: {
+        type: Number
+    },
+    name: {
+        type: String,
+        required: [true,'required name']
+    },
+    email: {
+        type: String,
+        match: [/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/, " Invalid Email"],
+        required: [true,'required email']
+
+    },
+    password: {
+        type: String,
+        required: [true,'required password']
+    },
+    phone: {
+        type: String,
+        match: [/^01[0125][0-9]{8}$/, "Invalid phoneNumber"],
+        required: [true,'required phone']
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'client'],
+        required: [true,'required role']
+    }
+}, {
+    timestamps: true
+});
+
+userSchema.plugin(Autoincrement,  {id: 'user_id',inc_field: 'id'});
+
+const users = mongoose.model('users', userSchema);
