57c2cdc3f53c54a04f36876973ae744e3f564751
SomayaRagab <eng.somayaragab@gmail.com>
2023-05-26 20:45:29 +0200

    update item schema

--- BackEnd/Models/categoryModel.js
+++ BackEnd/Models/categoryModel.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 const AutoIncrement = require("mongoose-sequence")(mongoose);
-const schema = new mongoose.Schema({
-  _id: Number,
-  name: { type: String, required: true },
-});
+const schema = new mongoose.Schema(
+  {
+    _id: Number,
+    name: { type: String, required: [true, "required name"] },
+  },
+  { timestamps: true }
+);
 
 schema.plugin(AutoIncrement, { id: "category_id", inc_field: "_id" });
 mongoose.model("categories", schema);
--- BackEnd/Models/itemModel.js
+++ BackEnd/Models/itemModel.js
@@ -1,18 +1,27 @@
 const mongoose = require("mongoose");
 const AutoIncrement = require("mongoose-sequence")(mongoose);
-const schema = new mongoose.Schema({
-  _id: Number,
-  name: { type: String, required: true },
-  qty: { type: Number, required: true },
-  images: { type: String, required: true },
-  material: { type: String, required: true },
-  size: { type: String, required: true },
-  color: { type: String, required: true },
-  deliveryTime: { type: Date },
-  shippingType: { type: String, enum: ["Percentage", "Amount"] },
-  estimatedShipping: { type: Date },
-  category: { type: Number, ref: "categories" },
-
-});
+const schema = new mongoose.Schema(
+  {
+    _id: Number,
+    name: { type: String, required: [true, "Name is required"] },
+    qty: { type: Number, required: [true, "Qty is required"] },
+    images: { type: String, required: [true, "Image is required"] },
+    material: { type: String, required: [true, "Material is required"] },
+    size: { type: String, required: [true, "Size is required"] },
+    color: { type: String, required: [true, "Color is required"] },
+    bidding_gap: {
+      type: Number,
+      required: [true, "Bidding Gap is required"],
+    },
+    start_bidding: {
+      type: Number,
+      required: [true, "Start Bidding is required"],
+    },
+    max_price: { type: Number, required: [true, "Max Price is required"] },
+    end_time: { type: Date, required: [true, "End Time is required"] },
+    category: { type: Number, ref: "categories" },
+  },
+  { timestamps: true }
+);
 schema.plugin(AutoIncrement, { id: "item_id", inc_field: "_id" });
 mongoose.model("items", schema);
