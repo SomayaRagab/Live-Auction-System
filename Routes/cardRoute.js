b126df0b85e7d34d9ae8bd69a3393b9741ae2538
SomayaRagab <eng.somayaragab@gmail.com>
2023-06-22 11:11:59 +0200

    payment stripe

--- Routes/cardRoute.js
+++ Routes/cardRoute.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../Controllers/cardController');
 
-router.route('/winners')
-    .get(controller.getAllWinner);
+router.route('/winners').get(controller.getAllWinner);
+router.route('/winners/items').get(controller.getWinnerItems);
 
-
+router
+  .route('/cards/:id')
+  // .get(controller.getAllItemDetails)
+  .delete(controller.deleteCard);
 
 module.exports = router;
