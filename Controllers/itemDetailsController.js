0827d5f449a9e56f56879fef27d83b43fe1c081b
Habiba Hamed <bibahamed99@gmail.com>
2023-06-24 04:54:26 +0300

    date

--- Controllers/itemDetailsController.js
+++ Controllers/itemDetailsController.js
@@ -18,7 +18,7 @@ exports.createItemDetails = async (req, res) => {
     if (!auction) throw new Error('المزاد غير موجود');
 
     // check start date item is greater than start date auction
-    if ((new Date(req.body.start_date) < new Date(auction.start_date))) {
+    if (new Date(req.body.start_date) < new Date(auction.start_date)) {
       throw new Error(
         'تاريخ بدايه المنتج يجب ان يكون نفس يوم المزاد او بعده من تاريخ بدايه المزاد'
       );
@@ -162,8 +162,9 @@ exports.getItemDetailsByAuctionId = async (req, res) => {
     const now = new Date();
     for (let itemDetails of itemsDetails) {
       if (
-        addDurationToDate(itemDetails.start_date, itemDetails.duration) >
-        new Date(Date.now())
+        addDurationToDate(itemDetails.start_date, itemDetails.duration) <
+          new Date(Date.now()) &&
+        itemDetails.start_date < now
       ) {
         itemDetails.is_open = false;
         await itemDetails.save();
